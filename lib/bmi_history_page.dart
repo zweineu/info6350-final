b48ef308d9204d1e7374d4cc235c9b3c6ffedca8
zwei <wei.zhong@northeastern.edu>
2024-04-20 15:11:17 -0700

    add chart in the history page

--- lib/bmi_history_page.dart
+++ lib/bmi_history_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 import 'bmi_record.dart';
 
@@ -24,17 +25,43 @@ class HistoryPage extends StatelessWidget {
           if (snapshot.hasError) {
             return Center(child: Text("Error: ${snapshot.error}"));
           }
-          return ListView.builder(
-            itemCount: snapshot.data?.length ?? 0,
-            itemBuilder: (context, index) {
-              BMIRecord record = snapshot.data![index];
-              return ListTile(
-                title: Text("BMI: ${record.bmi.toStringAsFixed(2)}"),
-                subtitle: Text(
-                    "Date: ${DateFormat('yyyy-MM-dd').format(record.date)}"),
-              );
-            },
-          );
+          return Column(children: [
+            //Initialize the chart widget
+            SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                // Chart title
+                title: ChartTitle(text: 'BMI History'),
+                // Enable legend
+                legend: Legend(isVisible: true),
+                // Enable tooltip
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <CartesianSeries<BMIRecord, String>>[
+                  LineSeries<BMIRecord, String>(
+                      dataSource: snapshot.data,
+                      xValueMapper: (BMIRecord records, _) =>
+                          DateFormat('yyyy-MM-dd HH:mm').format(records.date),
+                      yValueMapper: (BMIRecord records, _) => records.weight,
+                      name: 'Weight',
+                      // Enable data label
+                      dataLabelSettings: DataLabelSettings(isVisible: true))
+                ]),
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              //Initialize the spark charts widget
+              child: ListView.builder(
+                itemCount: snapshot.data?.length ?? 0,
+                itemBuilder: (context, index) {
+                  BMIRecord record = snapshot.data![index];
+                  return ListTile(
+                    title: Text("BMI: ${record.bmi.toStringAsFixed(2)}"),
+                    subtitle: Text(
+                        "Date: ${DateFormat('yyyy-MM-dd').format(record.date)}"),
+                  );
+                },
+              ),
+            )),
+          ]);
         },
       ),
     );
